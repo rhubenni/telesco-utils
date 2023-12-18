a08c9b5f1dbb92c3f301eb5203cb2d3e37d3722a
Rhubenni Telesco <rhubenni.telesco@gmail.com>
2023-12-18 16:02:23 -0300

    added python version support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="telesco_utils",
-    version="0.4",
+    version="0.4.2",
     packages=find_packages(),
     description='This package provides general-purpose functionality',
     author='Rhubenni Telesco',
